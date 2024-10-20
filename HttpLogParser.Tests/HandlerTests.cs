5726449083bc528b810b67958f35b7a6bd3b8dad
codeblitzio <blair@codeblitz.io>
2024-10-20 12:50:44 +1100

    Added handler unit tests

--- HttpLogParser.Tests/HandlerTests.cs
+++ HttpLogParser.Tests/HandlerTests.cs
@@ -11,24 +11,59 @@ namespace HttpLogParser.Tests;
 
 public class HandlerTests
 {
-    readonly ILogger<GetHttpLogReportHandler> _logger = new Mock<ILogger<GetHttpLogReportHandler>>().Object;
-    readonly ILoader _loader = new Mock<ILoader>().Object;
-    readonly IParser _parser = new Mock<IParser>().Object;
-    readonly IRepository _repository = new Mock<IRepository>().Object;
-    readonly IOptions<AppOptions> _options = Microsoft.Extensions.Options.Options.Create(new AppOptions());
+    readonly Mock<ILoader> _loader;
+    readonly Mock<ILogger<GetHttpLogReportHandler>> _logger;
+    readonly IOptions<AppOptions> _options;
+    readonly IParser _parser;
+    readonly IRepository _repository;
+
+    public HandlerTests()
+    {
+       // we'll mock the loader as this has an external dependency on the file system
+       _loader = new Mock<ILoader>();
+
+        // we'll mock the logger
+        _logger = new Mock<ILogger<GetHttpLogReportHandler>>();
+
+        // we'll mock the app options
+        _options = Options.Create(new AppOptions {  HttpLogUri = "" });
+
+        // let's not mock the parser or repository as these have no external dependencies
+        _parser = new RegexParser(new Mock<ILogger<RegexParser>>().Object);
+        _repository = new InMemoryRepository(new Mock<ILogger<InMemoryRepository>>().Object);
+    }
 
     [Fact]
     public async Task Handler_Returns_Expected_Report_Payload()
     {
         // arrange
-        var input = @"177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] ""GET /intranet-analytics/ HTTP/1.1"" 200 3574 ""-"" ""Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7""";
+        var data = new List<string>
+        {
+            @"177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] ""GET /index/ HTTP/1.1"" 200 3574 ""-"" ""Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7""" + Environment.NewLine,
+            @"177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] ""GET /index/ HTTP/1.1"" 200 3574 ""-"" ""Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7""" + Environment.NewLine,
+            @"177.71.128.21 - - [10/Jul/2018:22:21:28 +0200] ""GET /index/ HTTP/1.1"" 200 3574 ""-"" ""Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7""" + Environment.NewLine, 
+            @"177.71.128.22 - - [10/Jul/2018:22:21:28 +0200] ""GET /about/ HTTP/1.1"" 200 3574 ""-"" ""Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7""" + Environment.NewLine,
+            @"177.71.128.22 - - [10/Jul/2018:22:21:28 +0200] ""GET /about/ HTTP/1.1"" 200 3574 ""-"" ""Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7""" + Environment.NewLine, 
+            @"177.71.128.23 - - [10/Jul/2018:22:21:28 +0200] ""GET /home/ HTTP/1.1"" 200 3574 ""-"" ""Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7""" + Environment.NewLine,
+            @"177.71.128.23 - - [10/Jul/2018:22:21:28 +0200] ""GET /home/ HTTP/1.1"" 200 3574 ""-"" ""Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7""" + Environment.NewLine, 
+            @"177.71.128.24 - - [10/Jul/2018:22:21:28 +0200] ""GET /help/ HTTP/1.1"" 200 3574 ""-"" ""Mozilla/5.0 (X11; U; Linux x86_64; fr-FR) AppleWebKit/534.7 (KHTML, like Gecko) Epiphany/2.30.6 Safari/534.7"""  
+        };
 
-        var sut = new GetHttpLogReportHandler(_loader, _repository, _parser, _options, _logger);
+        _loader.Setup(_ => _.Load(It.IsAny<string>()))
+            .Returns(data);
+
+        var sut = new GetHttpLogReportHandler(_loader.Object, _repository, _parser, _options, _logger.Object);
 
         // act
         var result = await sut.Handle(new HttpLogReportQuery(), default);
 
         //assert
-        Assert.True(true);
+        Assert.Equal(4, result.UniqueIpAdresses);
+        Assert.Equal("177.71.128.21", result.MostActiveIps.ToArray()[0]);
+        Assert.Equal("177.71.128.22", result.MostActiveIps.ToArray()[1]);
+        Assert.Equal("177.71.128.23", result.MostActiveIps.ToArray()[2]);
+        Assert.Equal("/index/", result.MostVisitedUrls.ToArray()[0]);
+        Assert.Equal("/about/", result.MostVisitedUrls.ToArray()[1]);
+        Assert.Equal("/home/", result.MostVisitedUrls.ToArray()[2]); 
     }
 }
--- HttpLogParser/Program.cs
+++ HttpLogParser/Program.cs
@@ -1,34 +1,15 @@
-using Hellang.Middleware.ProblemDetails;
 using HttpLogParser.Loaders;
 using HttpLogParser.Models;
 using HttpLogParser.Parsers;
 using HttpLogParser.Repositories;
-using Microsoft.AspNetCore.Mvc;
 using MediatR;
-using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.Configure<AppOptions>(builder.Configuration.GetSection("App"));
-
-// configure exception handling middleware
-builder.Services.AddProblemDetails(options =>
-{
-    options.IncludeExceptionDetails = (ctx, ex) => false;
-    options.ShouldLogUnhandledException = (context, ex, problem) => false;
-    options.GetTraceId = ctx => null;
-
-    options.Map<Exception>(ex => new ProblemDetails
-    {
-        Title = "Internal Server Error",
-        Status = (int)HttpStatusCode.InternalServerError,
-        Detail = "Oops, something went wrong"
-    });
-});
 
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
 
@@ -38,9 +19,6 @@ builder.Services.AddTransient<IParser, RegexParser>();
 
 var app = builder.Build();
 
-app.UseProblemDetails();
-
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -49,9 +27,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapControllers();
-
-// use Minimal API endpoints
 app.MapGet("/HttpLogReport", async (IMediator mediator, ILogger<Program> logger, CancellationToken cancellationToken) =>
 {
     var result = await mediator.Send(new HttpLogReportQuery(), cancellationToken);
@@ -60,5 +35,4 @@ app.MapGet("/HttpLogReport", async (IMediator mediator, ILogger<Program> logger,
 .WithName("GetHttpLogReport")
 .WithOpenApi();
 
-// let's rock!
 app.Run();
